12e0f9947d759f3ac8c779629fdac658597d560a
weiPC <386888618@qq.com>
2021-01-29 13:40:29 +0800

    v1.0.6

--- dist/weapp.qrcode.js
+++ dist/weapp.qrcode.js
@@ -1,5 +1,5 @@
 /**
- * weapp.qrcode.js v1.0.4 (undefined)
+ * weapp.qrcode.js v1.0.6 (undefined)
  */
 
 (function (global, factory) {
@@ -1210,7 +1210,7 @@ function utf16to8(str) {
   return out;
 }
 
-function drawQrcode(options) {
+function drawQrcode(options, debug) {
   options = options || {};
   options = extend(true, {
     canvasId: 'myQrcode',
@@ -1219,6 +1219,7 @@ function drawQrcode(options) {
     width: 260,
     height: 260,
     padding: 20,
+    // paddingColor: '#ffffff', // 默认与background一致
     typeNumber: -1,
     correctLevel: QRErrorCorrectLevel.H,
     background: '#ffffff',
@@ -1237,10 +1238,16 @@ function drawQrcode(options) {
     return;
   }
 
-  createCanvas();
-  var qrcode = new QRCode(options.typeNumber, options.correctLevel);
-  qrcode.addData(utf16to8(options.text));
-  qrcode.make();
+  if (!options.paddingColor) options.paddingColor = options.background;
+
+  if (debug) {
+    var qrcode = new QRCode(options.typeNumber, options.correctLevel);
+    qrcode.addData(utf16to8(options.text));
+    qrcode.make();
+    return qrcode;
+  } else {
+    createCanvas();
+  }
 
   function createCanvas() {
     // create the qrcode itself
@@ -1256,7 +1263,7 @@ function drawQrcode(options) {
     const width = canvas.width;
 
     // 背景色
-    ctx.fillStyle = options.background;
+    ctx.fillStyle = options.paddingColor;
     ctx.fillRect(0, 0, width + options.padding * 2, width + options.padding * 2);
     ctx.stroke();
 
@@ -1266,12 +1273,10 @@ function drawQrcode(options) {
     // 开始画二维码
     for (var row = 0; row < qrcode.getModuleCount(); row++) {
       for (var col = 0; col < qrcode.getModuleCount(); col++) {
-        if (qrcode.isDark(row, col)) {
-          ctx.fillStyle = options.foreground;
-          var w = Math.ceil((col + 1) * tileW) - Math.floor(col * tileW);
-          var h = Math.ceil((row + 1) * tileW) - Math.floor(row * tileW);
-          ctx.fillRect(Math.round(col * tileW) + options.padding, Math.round(row * tileH) + options.padding, w, h);
-        }
+        ctx.fillStyle = qrcode.isDark(row, col) ? options.foreground : options.background;
+        var w = Math.ceil((col + 1) * tileW) - Math.floor(col * tileW);
+        var h = Math.ceil((row + 1) * tileW) - Math.floor(row * tileW);
+        ctx.fillRect(Math.round(col * tileW) + options.padding, Math.round(row * tileH) + options.padding, w, h);
       }
     }
     ctx.stroke();
